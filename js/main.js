dd3e0460e30e084052498c07f77d54a1aa724a7b
Bryan Godoy <bry.godoyr@gmail.com>
2025-03-07 21:08:49 -0300

    se añade css y se ajusta container de historial

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 
 //Codigo
 
+//botones
 let calcular = document.getElementById("btnCalcular");
+let borrarHistorial = document.getElementById("btnBorrarHistorial");
+
+//textbox
 let alto = document.getElementById("txtAlto");
 let ancho = document.getElementById("txtAncho");
 let largo = document.getElementById("txtLargo");
 let peso = document.getElementById("txtPeso");
-let resultado = document.getElementById("resultado");
 
+//H1
+let resultado = document.getElementById("resultado"); // Para mostrar el resultado de calcular
+
+//div
 let historial = document.getElementById("historial") // Div para guardar el historial de precios 
+let historialContainer = document.getElementById("historial-container");
 
+//array
 let historialPrecios = JSON.parse(localStorage.getItem("historialPrecios")) || [];
+
+
 
 verHistorial();
 
@@ -24,14 +35,16 @@ calcular.onclick = () => {
 
     let precio;
 
-    if(total<=20){
+    if(total<=30){
         precio = 10000;
-    } else if (total >= 21 && total <= 30) {
+    } else if (total >= 31 && total <= 50) {
         precio = 20000;
-    } else if (total >= 31 && total <= 40) {
+    } else if (total >= 51 && total <= 70) {
         precio = 30000;
-    } else {
+    } else if (total >= 71 && total <= 90) {
         precio = 40000;
+    } else {
+        precio = 50000;
     }
 
     historialPrecios.push(precio);
@@ -52,10 +65,19 @@ function limpiarCampos() {
 }
 
 function verHistorial () {
+
+    if (historialPrecios.length === 0) {
+        historialContainer.style.display = "none";
+        return;
+    }
+
+
     historial.innerHTML = "<h2>Historial de calculos</h2>"
 
+    historialContainer.style.display = "flex";
+
     historialPrecios.forEach((precio, index)=>{
-        historial.innerHTML += `<p> ${index+1}. precio $${precio}</p>`;
+        historial.innerHTML += `<p> ${index+1}. Precio: $${precio}</p>`;
     })
 }
 
@@ -71,6 +93,12 @@ function validarisNaN() {
     return true;
 }
 
+borrarHistorial.onclick = () => {
+    historialPrecios = [];
+    localStorage.removeItem("historialPrecios");
+    verHistorial();
+
+};
 
 
 
